ea2275b8e03de3fa24f60f6ee985becce60cff4d
kajakIYD <ficupaolo@gmail.com>
2019-06-30 18:12:31 +0200

    Add a bit more of explainantion in whole_identification_simulation_pipeline.py

--- whole_identification_simulation_pipeline.py
+++ whole_identification_simulation_pipeline.py
@@ -1,7 +1,7 @@
-# Generate signal for identification
+# Generate signal for identification (generate input_output_files_for_model.py)
 
-# Identify model and model_inverse
+# Identify model and model_inverse (invocation also present in generate_input_output_files_for_model)
 
-# Run simulation, acquire mses
+# Run simulation, acquire mses (simulation.py)
 
-# compare_mses
+# compare identification mses and run chosen (ex. best) simulation(compare_mses.py)
